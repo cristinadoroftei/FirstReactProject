9a2678a15b57e929b97b5452c521e911a997275f
Cristina <dorofteicristina19@gmail.com>
2020-07-13 13:47:59 +0200

    control when the useEffect should take place. In this case, only the first time when the component is rendered

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,7 +8,7 @@ const cockpit = (props) => {
     setTimeout(() => {
       alert('Saved data to cloud!')
     }, 1000)
-  }, [props.persons])
+  }, [])
     
     let assignedClasses = [];
     let btnClass = ''
