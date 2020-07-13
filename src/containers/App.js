888ef770584f77dab0f648763d5e4c0b6e375a07
Cristina <dorofteicristina19@gmail.com>
2020-07-13 15:39:42 +0200

    hoc file WithClass in the short, easy way

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Persons from '../components/Persons/Persons.js'
 import Cockpit from "../components/Cockpit/Cockpit.js"
+import WithClass from "../hoc/WithClass.js"
 
 class App extends Component {
   constructor(props) {
@@ -83,7 +84,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
         {this.state.showCockpit ? 
         <Cockpit 
@@ -92,7 +93,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/> : null }
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, Im a react app!') )
   }
